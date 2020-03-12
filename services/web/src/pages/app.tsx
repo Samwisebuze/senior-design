571750114c7407a976c0166dd64e3c43f7e24a85
Nick Rook <rooknj@outlook.com>
2020-03-12 18:41:20 -0400

    fix: added routing back to app

--- services/web/src/pages/app.tsx
+++ services/web/src/pages/app.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { Link, navigate } from "gatsby";
 
 import { Router } from "@reach/router";
-import RandomPerson from "../modules/RandomPerson";
 import AppLayout from "../components/AppLayout";
 import Default from "../modules/Default";
 import PrivateRoute from "../components/PrivateRoute";
-import Application from "../components/canvas/application";
-import BodyWidget from "../components/canvas/BodyWidget";
 
 const App = () => {
-  const app = new Application();
-
   return (
     <AppLayout>
-      <BodyWidget app={app} />
+      <Router style={{ height: "100%", width: "100%" }} basepath="/app">
+        <PrivateRoute component={Default} path="/" />
+      </Router>
     </AppLayout>
   );
 };
