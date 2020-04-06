970daa94e8cab03b846935eb33b7cc7bf53ed7d1
Nick Rook <rooknj@outlook.com>
2020-04-06 11:16:04 -0400

    chore: removed drawer from app layout

--- services/web/src/components/AppLayout.tsx
+++ services/web/src/components/AppLayout.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import { navigate } from "gatsby";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 import { isLoggedIn, logout } from "../util/auth";
@@ -71,38 +63,6 @@ const AppLayout: React.FC = ({ children }) => {
             ) : null}
           </StyledToolbar>
         </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.toolbar} />
-          <List>
-            {["Host", "Server", "Router", "Switch", "Firewall"].map(
-              (text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              )
-            )}
-          </List>
-          <Divider />
-          <List>
-            {["Connect"].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
         <main className={classes.content}>
           <div className={classes.toolbar} />
           {children}
